d3c9dbfa92d609f9e9737c8e48ba08efaecd1625
ikoo29 <166725592+ikoo29@users.noreply.github.com>
2024-04-13 02:05:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+from databases import Database
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
+
+# 데이터베이스 URL 설정
+DATABASE_URL = "postgresql://user:password@host:port/database_name"
 
 app = FastAPI()
-app.mount("/", StaticFiles(directory="public", html = True), name="static")
+
+@app.on_event("startup")
+async def startup():
+    # 데이터베이스에 연결
+    await database.connect()
+    print("데이터베이스 연결 성공")
+
+@app.on_event("shutdown")
+async def shutdown():
+    # 데이터베이스 연결 해제
+    await database.disconnect()
